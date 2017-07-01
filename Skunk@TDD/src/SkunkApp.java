4c5191581a664cc608c5a654065ecb79010ae8c2
Temitayo Akinsanmi <temitayoakinsanmi@10.66.5.135>
2017-07-01 14:42:24 -0500

    Junit Test 1

--- Skunk@TDD/src/SkunkApp.java
+++ Skunk@TDD/src/SkunkApp.java
@@ -1,5 +1,14 @@
 
 public class SkunkApp {
-
+              int NumberofPlayer;
+	public void getPlayerInfo() {
+		 NumberofPlayer= 2;
+		
+		
+	}
+     public int getNumberofPlayer(){
+    	 
+    	 return NumberofPlayer; 
+     }
 	//hello skunk
 }
--- Skunk@TDD/src/TestSkunkApp.java
+++ Skunk@TDD/src/TestSkunkApp.java
@@ -1,12 +1,19 @@
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSkunkApp {
+		private SkunkApp game;
+	@Before
+      public void setup(){
+    	  game=new SkunkApp();
+      }
+
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void canCallGetPlayerInfo(){
+		
+		game.getPlayerInfo();
 	}
 
 }
